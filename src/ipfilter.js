abe7031eccbadfe38cdddc4795525856ba85bf6c
Dan Forys <d@forys.co.uk>
2018-10-05 16:15:32 +0100

    WIP

--- src/ipfilter.js
+++ src/ipfilter.js
@@ -44,9 +44,15 @@ var IpDeniedError = require('./deniedError');
 module.exports = function ipfilter(ips, opts) {
   ips = ips || false;
 
-  var getIps = _.isFunction(ips) ? ips : function(){ return ips; };
-  var logger = function(message){ console.log(message);};
-  var settings = _.defaults( opts || {}, {
+  var getIps = _.isFunction(ips)
+    ? ips
+    : function() {
+      return ips;
+    };
+  var logger = function(message) {
+    console.log(message);
+  };
+  var settings = _.defaults(opts || {}, {
     mode: 'deny',
     log: true,
     logF: logger,
@@ -57,119 +63,126 @@ module.exports = function ipfilter(ips, opts) {
   });
 
   function getClientIp(req) {
-    var ipAddress;
+    var ipAddresses = [];
 
-    var headerIp = _.reduce(settings.allowedHeaders, function(acc, header){
-      var testIp = req.headers[header];
-      if(testIp != ''){
-        acc = testIp;
+    var headerIp = _.reduce(
+      settings.allowedHeaders,
+      function(acc, header) {
+        var testIp = req.headers[header];
+        if (testIp != '') {
+          acc = testIp;
+        }
+
+        return acc;
+      },
+      ''
+    );
+
+    if (headerIp) {
+      ipAddresses = headerIp.split(',');
+    }
+
+    if (ipAddresses.length === 0) {
+      ipAddresses = [req.connection.remoteAddress];
+    }
+
+    if (ipAddresses.length === 0) {
+      return ipAddresses;
+    }
+
+    ipAddresses = ipAddresses.map(function(ipAddress) {
+      if (iputil.isV6Format(ipAddress) && ~ipAddress.indexOf('::ffff')) {
+        ipAddress = ipAddress.split('::ffff:')[1];
       }
 
-      return acc;
-    },'');
+      if (iputil.isV4Format(ipAddress) && ~ipAddress.indexOf(':')) {
+        ipAddress = ipAddress.split(':')[0];
+      }
 
-    if(headerIp) {
-      var splitHeaderIp = headerIp.split(',');
-      ipAddress = splitHeaderIp[0];
-    }
+      return ipAddress;
+    });
 
-    if(!ipAddress) {
-      ipAddress = req.connection.remoteAddress;
-    }
-
-    if(!ipAddress){
-      return '';
-    }
-
-    if(iputil.isV6Format(ipAddress) && ~ipAddress.indexOf('::ffff')){
-      ipAddress = ipAddress.split('::ffff:')[1];
-    }
-
-    if(iputil.isV4Format(ipAddress) && ~ipAddress.indexOf(':')){
-      ipAddress = ipAddress.split(':')[0];
-    }
-
-    return ipAddress;
+    return ipAddresses;
   }
 
-  var matchClientIp = function(ip){
+  var matchClientIp = function(ip) {
     var mode = settings.mode.toLowerCase();
 
-    var result = _.invoke(getIps(),testIp,ip,mode);
+    var result = _.invoke(getIps(), testIp, ip, mode);
 
-    if(mode === 'allow'){
+    if (mode === 'allow') {
       return _.some(result);
-    }else{
+    } else {
       return _.every(result);
     }
   };
 
-  var testIp = function(ip,mode){
+  var testIp = function(ip, mode) {
     var constraint = this;
 
     // Check if it is an array or a string
-    if(typeof constraint === 'string'){
-      if(rangeCheck.validRange(constraint)){
-        return testCidrBlock(ip,constraint,mode);
-      }else{
-        return testExplicitIp(ip,constraint,mode);
+    if (typeof constraint === 'string') {
+      if (rangeCheck.validRange(constraint)) {
+        return testCidrBlock(ip, constraint, mode);
+      } else {
+        return testExplicitIp(ip, constraint, mode);
       }
     }
 
-    if(typeof constraint === 'object'){
-      return testRange(ip,constraint,mode);
+    if (typeof constraint === 'object') {
+      return testRange(ip, constraint, mode);
     }
   };
 
-  var testExplicitIp = function(ip,constraint,mode){
-    if(ip === constraint){
+  var testExplicitIp = function(ip, constraint, mode) {
+    if (ip === constraint) {
       return mode === 'allow';
-    }else{
+    } else {
       return mode === 'deny';
     }
   };
 
-  var testCidrBlock = function(ip,constraint,mode){
-    if(rangeCheck.inRange(ip, constraint)){
+  var testCidrBlock = function(ip, constraint, mode) {
+    if (rangeCheck.inRange(ip, constraint)) {
       return mode === 'allow';
-    }else{
+    } else {
       return mode === 'deny';
     }
   };
 
-  var testRange = function(ip,constraint,mode){
-    var filteredSet = _.filter(getIps(),function(constraint){
-      if(constraint.length > 1){
+  var testRange = function(ip, constraint, mode) {
+    var filteredSet = _.filter(getIps(), function(constraint) {
+      if (constraint.length > 1) {
         var startIp = iputil.toLong(constraint[0]);
         var endIp = iputil.toLong(constraint[1]);
         var longIp = iputil.toLong(ip);
-        return  longIp >= startIp && longIp <= endIp;
-      }else{
+        return longIp >= startIp && longIp <= endIp;
+      } else {
         return ip === constraint[0];
       }
     });
 
-    if(filteredSet.length > 0){
+    if (filteredSet.length > 0) {
       return mode === 'allow';
-    }else{
+    } else {
       return mode === 'deny';
     }
   };
 
-  var error = function(ip, next){
+  var error = function(ip, next) {
     var err = new IpDeniedError('Access denied to IP address: ' + ip);
     return next(err);
   };
 
   return function(req, res, next) {
-    if(settings.excluding.length > 0){
-      var results = _.filter(settings.excluding,function(exclude){
+    if (settings.excluding.length > 0) {
+      var results = _.filter(settings.excluding, function(exclude) {
         var regex = new RegExp(exclude);
         return regex.test(req.url);
       });
 
-      if(results.length > 0){
-        if(settings.log && settings.logLevel !== 'deny'){
+      if (results.length > 0) {
+        if (settings.log && settings.logLevel !== 'deny') {
           settings.logF('Access granted for excluded path: ' + results[0]);
         }
         return next();
@@ -177,8 +190,8 @@ module.exports = function ipfilter(ips, opts) {
     }
 
     var _ips = getIps();
-    if(!_ips || !_ips.length) {
-      if(settings.mode == 'allow'){
+    if (!_ips || !_ips.length) {
+      if (settings.mode == 'allow') {
         // ip list is empty, thus no one allowed
         return error('0.0.0.0/0', next);
       } else {
@@ -189,9 +202,9 @@ module.exports = function ipfilter(ips, opts) {
 
     var ip = settings.detectIp(req);
 
-    if(matchClientIp(ip,req)) {
+    if (matchClientIp(ip, req)) {
       // Grant access
-      if(settings.log && settings.logLevel !== 'deny') {
+      if (settings.log && settings.logLevel !== 'deny') {
         settings.logF('Access granted to IP address: ' + ip);
       }
 
@@ -199,7 +212,7 @@ module.exports = function ipfilter(ips, opts) {
     }
 
     // Deny access
-    if(settings.log && settings.logLevel !== 'allow') {
+    if (settings.log && settings.logLevel !== 'allow') {
       settings.logF('Access denied to IP address: ' + ip);
     }
 
